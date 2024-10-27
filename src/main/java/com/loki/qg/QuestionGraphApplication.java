0df5c3c8fbfefdcec59cfbdd2dcd245bea7cb77b
haiseong <wjdgotjd9908@gmail.com>
2024-10-27 22:26:51 +0900

    feat: 스프링 부트 프로젝트 초기 세팅

---
+++ src/main/java/com/loki/qg/QuestionGraphApplication.java
@@ -0,0 +1,13 @@
+package com.loki.qg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loki/qg/QuestionGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loki.qg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

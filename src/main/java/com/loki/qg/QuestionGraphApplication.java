59f6deb261e9bb1df45d3ea6233f482f7fc3a60a
haiseong <wjdgotjd9908@gmail.com>
2024-10-27 23:18:48 +0900

    feat: nodejs 프로젝트 초기 세팅

--- src/main/java/com/loki/qg/QuestionGraphApplication.java
+++ src/main/java/com/loki/qg/QuestionGraphApplication.java
@@ -3,11 +3,11 @@ package com.loki.qg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.loki.qg")
 public class QuestionGraphApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuestionGraphApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionGraphApplication.class, args);
+    }
 
 }

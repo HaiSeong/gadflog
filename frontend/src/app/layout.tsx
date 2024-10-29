ca104dee924f638f048c8cfec7b3efc3c25000c8
haiseong <wjdgotjd9908@gmail.com>
2024-10-30 03:44:09 +0900

    feat: 헤더 누르면 홈화면으로 이동

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {Toaster} from "@/components/ui/toaster";
+import Link from "next/link";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -14,7 +15,9 @@ export default function RootLayout({children,}: {
             <header className="sticky top-0 bg-white z-50 opacity-100">
                 <div className="grid grid-cols-10 gap-5">
                     <div className="col-start-4 col-span-4 pt-4 pb-4">
-                        <h1 className="text-3xl font-bold">GADFLOG</h1>
+                        <Link href="/" className="cursor-pointer hover:opacity-80 transition-opacity">
+                            <h1 className="text-3xl font-bold">GADFLOG</h1>
+                        </Link>
                     </div>
                 </div>
             </header>
